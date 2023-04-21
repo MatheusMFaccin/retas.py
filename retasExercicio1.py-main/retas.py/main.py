6f9f4152a3957a3e7b5a4ccef2265f495c8951b7
Matheus Machado Faccin <96211501+MatheusMFaccin@users.noreply.github.com>
2023-04-21 00:40:10 -0300

    Update main.py

--- retasExercicio1.py-main/retas.py/main.py
+++ retasExercicio1.py-main/retas.py/main.py
@@ -1,21 +1,13 @@
-#classe onde vai ficar armazenado os pontos x e y das retas 
-# e tambem vai fazer as operações para achar o coeficiente angular 
-# e os pontos de intesecção
 class Reta:
-    def __init__(self, x , y):
-        self.x = x
-        self.y = y
-        
-    #calcula a intersecção da seguinte forma:
-    #primeiro achamos o valor de x usando a seguinte formula " M1X1 + B1 = M2X2 + B2 "
-    # que vai resulta em " M1X1-M2X2 = B2 -B1 " e por fim vai resultar em " X = B/M "
-    # depois de acharmos o X eu subistitui o X na primeira equação no caso "M*valor de X + B"     
-    
+    def __init__(self, cordenadaX , cordenadaY):
+        self.cordenadaX = cordenadaX 
+        self.cordenadaY = cordenadaY
     def calcularInterseccao(retas):
         x = Reta.calcularX(retas)
         y = Reta.calcularY(x, retas)
         
-        print("pontos de intersecção: ( "+str(x)+" , "+str(y)+" )")    
+        print("pontos de intersecção: ( "+str(x)+" , "+str(y)+" )")  
+        return x , y  
         
     def calcularX(retas):
         coeficienteMX = []
@@ -23,8 +15,8 @@ class Reta:
         
         for reta in retas:
             print(retas)
-            coeficienteMX.append(reta.x)
-            coeficienteB.append(reta.y)
+            coeficienteMX.append(reta.cordenadaX)
+            coeficienteB.append(reta.cordenadaY)
         m = calcularCoeficiente(coeficienteMX[1],coeficienteMX[0])
         b = calcularCoeficiente(coeficienteB[0],coeficienteB[1])
         return b/m
@@ -32,7 +24,7 @@ class Reta:
     
     def calcularY(x, retas):
         
-        y = (retas[0].x*x)+retas[0].y
+        y = (retas[0].cordenadaX*x)+retas[0].cordenadaY
         return y 
     
    
@@ -40,30 +32,71 @@ class Reta:
     def coeficienteAngular(retas):
         coeficientes = []
         for reta in retas:
-            coeficientes.append (float(reta.x/reta.y))
+            if reta.cordenadaX == 0:
+                resultado = ""
+            elif reta.cordenadaY == 0 :
+                resultado = ""
+                
+            else:
+                resultado = reta.cordenadaX/reta.cordenadaY
+            coeficientes.append (resultado)
         return coeficientes
+
+class Restricao(Reta):
+    def __init__(self, cordenadaX , cordenaday, coeficiente):
+        super().__init__(cordenadaX,cordenaday)
+        self.coeficiente = coeficiente
+        
+    #calcula a intersecção da seguinte forma:
+    #primeiro achamos o valor de x usando a seguinte formula " M1X1 + B1 = M2X2 + B2 "
+    # que vai resulta em " M1X1-M2X2 = B2 -B1 " e por fim vai resultar em " X = B/M "
+    # depois de acharmos o X eu subistitui o X na primeira equação no caso "M*valor de X + B"
+    
+    
     
 def calcularCoeficiente(coeficiente1,coeficient2):
     return coeficiente1 - coeficient2
           
-           
+def acharInterseccao(coeficientes, retas):
+    for i in range(len(coeficientes)):  
+      print("contador: "+str(i)+" coeficiente: "+str(coeficientes[i]))
+      for j in range(len(coeficientes)): 
+        print("contador: "+str(j)+" coeficiente: "+str(coeficientes[j]))
+        if i == j:
+          print("")
+        elif coeficientes[i] == coeficientes[j]:
+          print("as retas são paralelas")
+        else:
+          retasInterseccao = []
+          pontos = []
+          print("as retas não são paralelas")
+          retasInterseccao.append(retas[i])
+          retasInterseccao.append(retas[j])
+          
+          x , y = Reta.calcularInterseccao(retasInterseccao)
+          pontos.append(Reta(x,y))
+    return pontos          
 
 
 i = 0
-retas  = []
+restricoes  = []
 coeficientes = []
 # input para o usuario escolher os valores das retas NÃO DIGITE LETRAS DIGITE APENAS VALORES 
-while i < 2:       
-    x = float(input("defina o valor de x da reta "+str(i+1)+" : "))
-    y = float(input("defina o valor de y da reta"+str(i+1)+" : "))
-    retas.append(Reta(x,y))
-    print("x: " +str(retas[i].x) + " y: "+ str(+retas[i].y))
+numeroRetas = int(input("quantas retas você quer criar ? "))
+i = 0
+while i < numeroRetas:        
+    x = float(input("defina o valor de x da reta "+str(i+1)+" :  "))
+    y = float(input("defina o valor de y da reta "+str(i+1)+" :  "))
+    coeficiente = float(input("defina o valor do coeficiente da reta"+str(i+1)+" : "))
+    restricoes.append(Restricao(x,y,coeficiente))
+    print("x: " +str(restricoes[i].cordenadaX) + " y: "+ str(+restricoes[i].cordenadaY)+"  restrição: "+ str(+restricoes[i].coeficiente))
     i+=1
     
-coeficientes = Reta.coeficienteAngular(retas)
+coeficientes = Reta.coeficienteAngular(restricoes)
+pontos = acharInterseccao(coeficientes, restricoes)
+for ponto in pontos:
+    print(ponto.cordenadaX,ponto.cordenadaY)
 
-if coeficientes[0] == coeficientes[1]:
-    print("as retas são paralelas") 
-else:
-    print("as retas não são paralelas")
-    Reta.calcularInterseccao(retas)
+
+
+w
